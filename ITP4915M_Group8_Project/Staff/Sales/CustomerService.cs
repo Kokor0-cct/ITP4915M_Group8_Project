3cd315a2db322de3f3e2386995d40c628860d24b
jackie0221 <jackie20070220@gmail.com>
2026-06-01 21:39:04 +0800

    addlal OderInfrommationEdit

---
+++ ITP4915M_Group8_Project/Staff/Sales/CustomerService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Staff
+{
+    public partial class CustomerService : Form
+    {
+        public CustomerService()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCID1001_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            Staff staff = new Staff();
+            staff.Show();
+            this.Hide();
+        }
+
+        private void btnCID1007_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
---
+++ ITP4915M_Group8_Project/Staff/Sales/OderInfrommationEdit.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Staff
+{
+    public partial class OderInfrommationEdit : Form
+    {
+        public OderInfrommationEdit()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            
+            this.Hide();
+        }
+
+        private void btnDetial1_Click1(object sender, EventArgs e)
+        {
+            OderInfrommation oderInfrommation = new OderInfrommation();
+            oderInfrommation.Show();
+            this.Hide();
+        }
+    }
+}
