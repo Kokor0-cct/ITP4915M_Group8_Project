1756fbb2934f0511719663c1b53764e825849e6c
jackie0221 <jackie20070220@gmail.com>
2026-06-16 11:30:36 +0800

    add Material Requirement Sheet

---
+++ ITP4915M_Group8_Project/Staff/Production Departmennt/Material Requirement Sheet/Material Requirement Sheet.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Material_Requirement_Sheet
+{
+    public partial class Material_Requirement_Sheet : UserControl
+    {
+        public Material_Requirement_Sheet()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ITP4915M_Group8_Project/Staff/Production Departmennt/Material Requirement Sheet/MaterialRequirementSheet.cs
@@ -0,0 +1,29 @@
+namespace Material_Requirement_Sheet
+{
+    public partial class FMaterialRequirementSheet : Form
+    {
+        public FMaterialRequirementSheet()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void btnADDMaterialRequirement_Click(object sender, EventArgs e)
+        {
+            fpMaterialRequirement.Controls.Add(new Material_Requirement_Sheet());
+        }
+
+        private void btndeleteMaterialRequirement_Click(object sender, EventArgs e)
+        {
+            
+            if (fpMaterialRequirement.Controls.Count == 0)
+            {
+                System.Windows.Forms.MessageBox.Show("No more material requirement sheet to delete.");
+                return;
+            }
+
+            fpMaterialRequirement.Controls.RemoveAt(fpMaterialRequirement.Controls.Count - 1);
+        }
+    }
+}
